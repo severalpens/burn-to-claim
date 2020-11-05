4d97d5c1355841f2b290f347d46585d1f12e6087
severalpens <paulcollinsaf@gmail.com>
2020-11-05 20:15:15 +1000

    main

--- test/test.js
+++
@@ -1,8 +0,0 @@
-var ethers = require('ethers');
-
-// node test.js
-let provider = ethers.getDefaultProvider('ropsten');
-
-provider.getBlockNumber().then((blockNumber) => {
-    console.log("Current block number: " + blockNumber);
-});
